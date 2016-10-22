82a8fb96318c52ace1076d4f387fb85b5bbe8ca7
lgroleaz <lucasgroleaz@live.fr>
2016-10-22 12:03:16 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
+#include "Grille.hpp"
 #include <iostream>
+
 using namespace std;
-int main()
-{
-cout<<"Hello World"<<endl;
-cout<<"Appuyez sur entree"<<endl;
-cin.get();
-return 0;
+
+int main(){
+
+	int t, rCapt, rCom;
+	cin>>t>>rCapt>>rCom;
+
+
+	Grille * g=new Grille(t, rCapt, rCom);
+	g->rendRealisable();
+	cout<<"Borne sup : "<<g->getNbCapteurs()<<endl;
+
+	return 0;
+
 }
+
+
